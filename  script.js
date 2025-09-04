f7182c6d895b7bbcbf537d72b4eccfb39c738030
ylmievitori <emily.coutinho.toledo@escola.pr.gov.br>
2025-09-04 09:05:12 -0300

    ok

---
+++  script.js
@@ -0,0 +1,19 @@
+const avanca = document.querySelectorAl('.btn-proximo');
+const reiniciarBtn = document.getElementById('.btn-reinicia')
+
+vanca.forEach(button => {
+button.addEventLitener('clik', function(){
+      
+    const atual = document.querySelector('.ativo');
+proximoPasso= 'passo-'+this.getAttribute('data-proximo');
+
+
+}
+
+})
+
+
+
+
+
+
