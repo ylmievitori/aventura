98ce50d80aad5e48c0b92abe72d0d5c9ceccb6e0
ylmievitori <emily.coutinho.toledo@escola.pr.gov.br>
2025-09-11 09:06:19 -0300

    ok

---  script.js
+++  script.js
@@ -21,7 +21,14 @@ if (proximoElemento){
 }
 
 });
-
+// Reinicia o jogo ao clicar no botão de reinício
+if (reiniciarBtn) {
+    reiniciarBtn.addEventListener('click', () => {
+        const atual = document.querySelector('.ativo');
+        atual.classList.remove('ativo');
+        document.getElementById('passo-0').classList.add('ativo');
+    });
+}
 
 
 
